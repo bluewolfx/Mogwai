80954c24d16bda5a1298cc1d8722047ccd761963
ricardobanon <ricardobanon@gmail.com>
2022-10-23 03:47:11 +0100

    Created Structure -Index.js (Runs app) -deploy-commands.js (to deploy all commands created) -server.js -options-info.json (To display all the functions of the server) -package.json -package-lock.json Created commands -Ping,js (Bot replys pong) -Server (Bot replys with the info of the server) -User (Bot replys with ephemeral)

---
+++ events/interactionCreate.js
@@ -0,0 +1,26 @@
+const { Events } = require('discord.js');
+
+module.exports = {
+	name: Events.InteractionCreate,
+	async execute(interaction) {
+		if (!interaction.isChatInputCommand()) return;
+		const command = interaction.client.commands.get(interaction.commandName);
+
+		if (!command) {
+			console.error(`No command matching ${interaction.commandName} was found.`);
+			return;
+		}
+
+		try {
+            //Ephemeral response 
+            if (interaction.commandName === 'ping') {
+                await interaction.reply({ content: 'Secret Pong!', ephemeral: true });
+            }else{
+                await command.execute(interaction);
+            }
+		} catch (error) {
+			console.error(`Error executing ${interaction.commandName}`);
+			console.error(error);
+		}
+	},
+};
